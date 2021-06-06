56f5d05a16fe5a756305920f5029bd216b4efa32
이휘련 <dlgnlfus@gmail.com>
2021-06-06 23:57:56 +0900

    EventArgs 벤치마크 테스트

---
+++ Benchmark/EventArgsTest.cs
@@ -0,0 +1,55 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+
+namespace Benchmark {
+
+    [MarkdownExporter]
+    [MemoryDiagnoser]
+    public class EventArgsTest {
+        public class ClassEventTestArgs : EventArgs {
+            public int Handle;
+        }
+        public struct StructEventTestArgs {
+            public int Handle;
+        }
+        public delegate void ClassEvent(object sender, ClassEventTestArgs e);
+        public delegate void StructEvent(object sender, in StructEventTestArgs e);
+        public event ClassEvent OnClassEvent;
+        public event StructEvent OnStructEvent;
+        public const int Count = 1000;
+        public EventArgsTest()
+        {
+            OnClassEvent += ClassCallback;
+            OnStructEvent += StructCallback;
+        }
+
+        [Benchmark]
+        public void ClassEventArgsTest()
+        {
+            for (int i = 0; i < Count; i++) {
+                OnClassEvent.Invoke(this, new ClassEventTestArgs {
+                    Handle = i
+                });
+            }
+        }
+        void ClassCallback(object sender, ClassEventTestArgs e)
+        {
+
+        }
+
+        [Benchmark]
+        public void StructEventArgsTest()
+        {
+            for (int i = 0; i < Count; i++) {
+                OnStructEvent.Invoke(this, new StructEventTestArgs {
+                    Handle = i
+                });
+            }
+        }
+
+        public void StructCallback(object sender, in StructEventTestArgs e)
+        {
+
+        }
+    }
+}
---
+++ Benchmark/Program.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace Benchmark {
+    internal class Program {
+        private static void Main(string[] args)
+        {
+#if DEBUG
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).RunAll(new DebugInProcessConfig());
+#elif RELEASE
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).RunAll();
+#endif
+        }
+    }
+}
